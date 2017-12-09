b063735de3e5b82938732e1c7f7e946fa0affc7e
jeffwdoak <jeff.w.doak@gmail.com>
2017-12-09 11:01:23 -0600

    cleaned up atomic_participation_ratio.py

--- PhononModes/atomic_participation_ratio.py
+++ PhononModes/atomic_participation_ratio.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
 
-from phononmodes import *
-import numpy as np
+"""
+Script to calculate atom-decomposed participation ratios of a phonon
+calculation.
+
+Assumes the phonon mode file is named 'phonons.out', the atomic mass file is
+named 'apos.dat', and both are located in the directory where this script is
+run.
+
+Script takes 1 command line argument, the index of the phonon mode for which to
+calculate the atom-decomposed participation ratio.
+
+Prints atom-decomposed participation ratio to standard output.
+
+"""
+
 import sys
 
+from phononmodes import *
 
 modes = PhononModes()
 
@@ -11,8 +25,8 @@ index = int(sys.argv[1])
 
 aprs = modes.atomic_participation_ratio(index)
 
-print "Number of atoms:",modes.num_atoms
-print "Number of modes:",modes.num_modes
+print "Number of atoms:", modes.num_atoms
+print "Number of modes:", modes.num_modes
 print "Participation Ratio from Eq. (1):"
 print modes.participation_ratio(index)
 print "Participation Ratio from Sum over Eq. (2):"
@@ -20,5 +34,4 @@ print aprs.sum()**2
 print
 print "Mode_# Atom_# Atomic_Participation_Ratio"
 for j in range(modes.num_atoms):
-    print index,j+1,aprs[j]
-
+    print index, j+1, aprs[j]
