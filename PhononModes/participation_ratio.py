eb3a262da3128e162f99687877179b9631cda2b6
jeffwdoak <jeff.w.doak@gmail.com>
2017-12-09 09:20:22 -0600

    cleaned up participation_ratio.py

--- PhononModes/participation_ratio.py
+++ PhononModes/participation_ratio.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
-from phononmodes import *
-import numpy as np
-import sys
+"""
+Script to calculate and output to screen the participation ratios of a phonon
+calculation.
 
+Assumes the phonon mode file is named 'phonons.out', the atomic mass file is
+named 'apos.dat', and both are located in the directory where this script is
+run.
+
+"""
+
+from phononmodes import *
 
 modes = PhononModes()
 
 print "Mode_# Frequency_(cm^-1) Participation_Ratio"
 for i in range(modes.num_modes):
-    print i,modes.freqs[i],modes.participation_ratio(i)
-
+    print i, modes.freqs[i], modes.participation_ratio(i)
